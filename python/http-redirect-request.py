c575411362f6009c19468bf97f2d8181d9bae426
Baui <github@bauknecht.email>
2024-06-07 19:18:50 +0200

    Update http-redirect-request.py

--- python/http-redirect-request.py
+++ python/http-redirect-request.py
@@ -7,8 +7,11 @@ def request(flow: http.HTTPFlow) -> None:
     # pretty_host takes the "Host" header of the request into account,
     # which is useful in transparent mode where we usually only have the IP
     # otherwise.
-    if flow.request.pretty_host == "api.lidarr.audio" or flow.request.pretty_host == "ws.audioscrobbler.com":
-        flow.request.headers["X-Proxy-Host"] = flow.request.pretty_host
-        flow.request.scheme = "http"
-        flow.request.host = "127.0.0.1"
-        flow.request.port = 7171
+# Check if the request host is the specific URL and do nothing if it is
+if flow.request.pretty_host == "https://api.lidarr.audio/api/v0.4/spotify/":
+    pass
+# Otherwise, check the original conditions and modify the request as needed
+elif flow.request.pretty_host == "api.lidarr.audio" or flow.request.pretty_host == "ws.audioscrobbler.com":
+    flow.request.headers["X-Proxy-Host"] = flow.request.pretty_host
+    flow.request.scheme = "http"
+    flow.request.host = "127.0.0.1"
